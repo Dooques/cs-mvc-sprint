ce31f82d210100ff334545ac3029a542d2275efa
Dooques <samuelgdukes@gmail.com>
2026-04-24 11:44:15 +0100

    Add project

---
+++ cs-mvc-sprint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cs_mvc_sprint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
